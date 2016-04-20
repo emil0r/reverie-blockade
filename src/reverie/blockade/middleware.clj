a7627ea15ef24bce688b10014136062944086206
Emil Bengtsson <emil@emil0r.com>
2016-04-20 23:44:58 +0200

    memoize domains in order to avoid hitting the database

--- src/reverie/blockade/middleware.clj
+++ src/reverie/blockade/middleware.clj
@@ -1,9 +1,7 @@
 (ns reverie.blockade.middleware
-  (:require [clojure.string :as str]
-            [ez-database.core :as db]
-            [yesql.core :refer [defqueries]]))
-
-(defqueries "queries/modules/blockade.sql")
+  (:require [clojure.core.memoize :as memo]
+            [clojure.string :as str]
+            [ez-database.core :as db]))
 
 (defn- get-domain [referer]
   (if referer
@@ -16,17 +14,31 @@
         first)
     (:remote-addr request)))
 
+(defn- get-domains* [db]
+  (let [domains (db/query db {:select [:domain :ip]
+                              :from [:blockade_list]
+                              :where [:= :active_p true]})]
+    (println "get-domains*")
+    (->> domains
+         (map (fn [{:keys [domain ip]}]
+                [domain ip]))
+         (flatten)
+         (remove str/blank?)
+         (into #{}))))
+
+;; avoid hitting the database all the time
+(def get-domains (memo/ttl get-domains* :ttl/threshold (* 60 1000)))
+
 (defn wrap-blockade [handler & [opts]]
-  (fn [request]
-    (let [db (get-in request [:reverie :database])
-          domain (get-domain (get-in request [:headers "referer"]))
+  (fn [{{db :database} :reverie :as request}]
+    (let [domain (get-domain (get-in request [:headers "referer"]))
           ip (get-ip request)
-          blocked? (->> (db/query db sql-is-blocked?
-                                  {:domain domain
-                                   :ip ip})
-                        first :blocked_p)]
+          domains (get-domains db)
+          blocked? (or (contains? domains domain)
+                       (contains? domains ip))]
       (if blocked?
         (do (when (:log? opts)
+              ;; this should go into a queue of some sort
               (db/query! db
                          {:insert-into :blockade_stats
                           :values [{:ip ip
